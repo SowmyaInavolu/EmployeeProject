56c04fef0f121d4748ee62c8f197d17e4603b5d9
sowmya.inavolu <sowmya.inavolu@acsicorp.com>
2023-07-04 12:04:02 +0530

    Removed the commented code in employeeserviceimplementation

--- src/main/java/com/employee/service/impl/EmployeeServiceImplementation.java
+++ src/main/java/com/employee/service/impl/EmployeeServiceImplementation.java
@@ -54,17 +54,6 @@ public class EmployeeServiceImplementation implements EmployeeService {
 
 	@Override
 	public Employee updateEmployee(long id, Employee updatedEmployee) {
-//		if (employee != null) {
-//
-//			updatedEmployee.forEach((key, value) -> {
-//				Field field = ReflectionUtils.findField(Employee.class, (String) key);
-//				assert field != null;
-//				field.setAccessible(true);
-//				ReflectionUtils.setField(field, employee, value);
-//			});
-//			employeeRepository.save(employee);
-//		}
-
 		Employee oldEmployeeData = employeeRepository.findById(id).orElse(null);
 		if (oldEmployeeData != null) {
 			oldEmployeeData.setEmployeeName(updatedEmployee.getEmployeeName());
